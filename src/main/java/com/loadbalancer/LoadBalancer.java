8e5e0a1154738cf4ba46d8bdfb937afe039b8a98
nhogg <nathanhogg1223@gmail.com>
2025-07-04 15:28:23 -0500

    Initial Outlining and planning of LoadBalancer.java and HealthChecker.java

--- src/main/java/com/loadbalancer/LoadBalancer.java
+++ src/main/java/com/loadbalancer/LoadBalancer.java
@@ -1,4 +1,30 @@
 package com.loadbalancer;
 
+import com.loadbalancer.health.HealthChecker;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class LoadBalancer {
+    private final ConcurrentHashMap<BackendServer, Boolean> serverList = new ConcurrentHashMap<>();
+    private HealthChecker healthChecker;
+
+    public void start() {
+        // Needs to call checkHealth immediately
+        // Needs to initialize serverList from config/servers.json
+        // Needs to awaken both threads
+    }
+
+    private ConcurrentHashMap<BackendServer, Boolean> checkHealth() {
+        // Should call healthChecker.checkHealth() to retrieve the health status of each server
+        // Should update serverList with the health status
+        //
+    }
+
+    private void route(Request request) {
+        // Should do other operations to route the request
+    }
+
+
+
+
 }
--- src/main/java/com/loadbalancer/health/HealthChecker.java
+++ src/main/java/com/loadbalancer/health/HealthChecker.java
@@ -1,4 +1,23 @@
 package com.loadbalancer.health;
 
+import com.loadbalancer.BackendServer;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class HealthChecker {
+    private final ConcurrentHashMap<BackendServer, Boolean> serverList;
+    private final long checkInterval;
+
+    public HealthChecker(ConcurrentHashMap<BackendServer, Boolean> serverList, long checkInterval) {
+        this.serverList = serverList;
+    }
+
+    public void run() {
+        for (BackendServer server : serverList.keySet()) {
+            boolean isHealthy = ping(server);
+            serverList.put(server, isHealthy);
+        }
+    }
+
+
 }
